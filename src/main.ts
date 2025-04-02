8ec36e32deb691044938fc7d123a2a165458f24f
Vincent Legout <vincent@legout.info>
2025-04-02 21:09:53 +0200

    migrate to react

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Hello World!</h1>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
