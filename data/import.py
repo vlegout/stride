dea376d9c5a8e5624d5e7154df6c03828f2fe822
Vincent Legout <vincent@legout.info>
2025-04-03 07:35:13 +0200

    improve import

--- data/import.py
+++ data/import.py
@@ -7,7 +7,9 @@
 # ///
 
 
+import datetime
 import json
+import os
 import uuid
 
 from typing import Optional
@@ -20,26 +22,42 @@ from pydantic import BaseModel
 
 class Activity(BaseModel):
     fit: str
-    sport: Optional[str] = ""
+    sport: str = None
+    start_time: datetime.datetime = None
+    total_distance: float = 0.0
+
+class Activities(BaseModel):
+    activities: list[Activity]
 
 
 def run():
-    with open("2025/04/01.yaml", "r") as file:
-        config = yaml.safe_load(file)
+    activities = Activities(activities=[])
 
-    with fitdecode.FitReader("2025/04/" + config["fit"]) as fit:
-        activity = Activity(fit=config["fit"])
+    yaml_files = []
+    for root, _, files in os.walk("."):
+        for file in files:
+            if file.endswith(".yaml"):
+                yaml_files.append(os.path.join(root, file))
 
-        for frame in fit:
-            if frame.frame_type == fitdecode.FIT_FRAME_DATA:
-                    if frame.name == "sport":
-                        for field in frame.fields:
-                            if field.name == "sport":
-                                activity.sport = field.value
-        print(activity)
+    for yaml_file in yaml_files:
+        with open(yaml_file, "r") as file:
+            config = yaml.safe_load(file)
+   
+        with fitdecode.FitReader(config["fit"]) as fit:
+            activities.activities.append(Activity(fit=config["fit"]))
 
-        with open("../src/data.json", "w") as file:
-            json.dump(activity.model_dump(), file, indent=4)
+            for frame in fit:
+                if frame.frame_type == fitdecode.FIT_FRAME_DATA and frame.name == "session":
+                    for field in frame.fields:
+                        if field.name == "sport":
+                            activities.activities[0].sport = field.value
+                        if field.name == "start_time":
+                            activities.activities[0].start_time = field.value
+                        if field.name == "total_distance":
+                            activities.activities[0].total_distance = field.value
+
+    with open("../public/data.json", "w") as file:
+        json.dump(activities.model_dump(), file, default=str)
 
 
 if __name__ == "__main__":
