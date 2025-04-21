bfe20de93bcf3a7c18e59872c3d1cea24a1e9887
Vincent Legout <vincent@legout.info>
2025-04-21 14:36:25 +0200

    add build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+aws s3 cp -r s3://vlegout-sport/data ./public
+
+npm run build
