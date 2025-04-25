03c2de2ef7dff8e854cb4979b171ec37f1746566
Vincent Legout <vincent@legout.info>
2025-04-25 19:33:15 +0200

    set header background color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Box px={4}>
+    <Box px={4} bgColor="gray.200">
       <Flex h={16}>
         <HStack>
           <Box>
