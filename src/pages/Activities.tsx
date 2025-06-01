1e5e273e3b0f41be2a16f3248aa5ff813b5c8f85
Vincent Legout <vincent@legout.info>
2025-06-01 15:53:00 +0200

    move activities to mui

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import { Box, Flex } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { FormControl, InputLabel, MenuItem, Slider } from "@mui/material";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -36,76 +38,74 @@ const ActivitiesPage = () => {
   if (isPending || isFetching || error) return "Loading...";
 
   return (
-    <Flex justifyContent="center">
-      <Box>
-        <Flex marginBottom="10px">
-          <Box flex="1" marginRight="10px">
-            <FormControl fullWidth>
-              <InputLabel id="activities-sport">Sport</InputLabel>
-              <Select labelId="activities-sport" value={sport} label="Sport" onChange={handleChange}>
-                <MenuItem value={""}>All</MenuItem>
-                <MenuItem value={"cycling"}>Cycling</MenuItem>
-                <MenuItem value={"running"}>Running</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-          <Box flex="1" marginLeft="10px">
-            <FormControl fullWidth>
-              Distance
-              <Box marginLeft="10px" marginRight="10px">
-                <Slider
-                  getAriaLabel={() => "Minimum distance"}
-                  value={distance}
-                  onChangeCommitted={handleDistanceChange}
-                  valueLabelDisplay="auto"
-                  disableSwap
-                  min={0}
-                  max={100}
-                />
-              </Box>
-            </FormControl>
-          </Box>
-        </Flex>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Sport</TableCell>
-                <TableCell>Title</TableCell>
-                <TableCell>Location</TableCell>
-                <TableCell>Start Time</TableCell>
-                <TableCell>Distance</TableCell>
-                <TableCell>Average Speed</TableCell>
-                <TableCell>Total Ascent</TableCell>
-                <TableCell>Calories</TableCell>
-                <TableCell>Training Effect</TableCell>
-                <TableCell>Device</TableCell>
+    <Container>
+      <Grid container spacing={2} marginTop="20px" marginBottom="20px">
+        <Grid size={4}>
+          <FormControl fullWidth>
+            <InputLabel id="activities-sport">Sport</InputLabel>
+            <Select labelId="activities-sport" value={sport} label="Sport" onChange={handleChange}>
+              <MenuItem value={""}>All</MenuItem>
+              <MenuItem value={"cycling"}>Cycling</MenuItem>
+              <MenuItem value={"running"}>Running</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid size={8}>
+          <FormControl fullWidth>
+            Distance
+            <Box marginLeft="10px" marginRight="10px">
+              <Slider
+                getAriaLabel={() => "Minimum distance"}
+                value={distance}
+                onChangeCommitted={handleDistanceChange}
+                valueLabelDisplay="auto"
+                disableSwap
+                min={0}
+                max={100}
+              />
+            </Box>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Sport</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell>Start Time</TableCell>
+              <TableCell>Distance</TableCell>
+              <TableCell>Average Speed</TableCell>
+              <TableCell>Total Ascent</TableCell>
+              <TableCell>Calories</TableCell>
+              <TableCell>Training Effect</TableCell>
+              <TableCell>Device</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((activity) => (
+              <TableRow key={activity.id}>
+                <TableCell>
+                  <SportLogo sport={activity.sport} width={25} />
+                </TableCell>
+                <TableCell>
+                  <Link to={`/activities/${activity.id}`}>{activity.title}</Link>
+                </TableCell>
+                <TableCell>{activity.location}</TableCell>
+                <TableCell>{formatDateTime(activity.start_time)}</TableCell>
+                <TableCell>{formatDistance(activity.total_distance)}</TableCell>
+                <TableCell>{formatSpeed(activity.avg_speed)}</TableCell>
+                <TableCell>{activity.total_ascent}</TableCell>
+                <TableCell>{activity.total_calories}</TableCell>
+                <TableCell>{activity.total_training_effect}</TableCell>
+                <TableCell>{activity.device}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((activity) => (
-                <TableRow key={activity.id}>
-                  <TableCell>
-                    <SportLogo sport={activity.sport} width={25} />
-                  </TableCell>
-                  <TableCell>
-                    <Link to={`/activities/${activity.id}`}>{activity.title}</Link>
-                  </TableCell>
-                  <TableCell>{activity.location}</TableCell>
-                  <TableCell>{formatDateTime(activity.start_time)}</TableCell>
-                  <TableCell>{formatDistance(activity.total_distance)}</TableCell>
-                  <TableCell>{formatSpeed(activity.avg_speed)}</TableCell>
-                  <TableCell>{activity.total_ascent}</TableCell>
-                  <TableCell>{activity.total_calories}</TableCell>
-                  <TableCell>{activity.total_training_effect}</TableCell>
-                  <TableCell>{activity.device}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
-    </Flex>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
