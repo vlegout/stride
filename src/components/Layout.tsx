a3e7a072bcb8e7e14fe429a3774805bd14c1400f
Vincent Legout <vincent@legout.info>
2025-06-01 07:33:52 +0200

    remove chakra in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Box, Grid } from "@chakra-ui/react";
 
 import Header from "../components/Header";
 
 const Layout = () => {
   return (
-    <Grid>
-      <main>
-        <Header />
-        <Box marginTop="20px">
-          <Outlet />
-        </Box>
-      </main>
-    </Grid>
+    <main>
+      <Header />
+      <Outlet />
+    </main>
   );
 };
 
