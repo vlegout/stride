42d054ffdd6fbd0684e03ea8617c9137819b2c3e
Vincent Legout <vincent@legout.info>
2025-06-01 15:43:13 +0200

    move profile to mui

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -49,77 +50,73 @@ const Profile = () => {
   };
 
   return (
-    <Flex justifyContent="center">
-      <Box maxWidth="1200px">
-        <Box>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableBody>
-                <TableRow>
-                  <TableCell>Total Activities</TableCell>
-                  <TableCell>{data.n_activities}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Run Total Activities</TableCell>
-                  <TableCell>{data.run_n_activities}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Run Total Distance</TableCell>
-                  <TableCell>{formatDistance(data.run_total_distance)}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Cycling Total Activities</TableCell>
-                  <TableCell>{data.cycling_n_activities}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Cycling Total Distance</TableCell>
-                  <TableCell>{formatDistance(data.cycling_total_distance)}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-        <Box marginTop="20px">
-          <Bar height={"100px"} options={weekOptions} data={weekData} />
-        </Box>
-        <Flex justifyContent="center" paddingTop="20px" flexDirection="column" alignItems="center">
-          <Box>
-            <Performances performances={data.running_performances} />
-          </Box>
-        </Flex>
-        <Box marginTop="20px">
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Year</TableCell>
-                  <TableCell colSpan={2}>Running</TableCell>
-                  <TableCell colSpan={2}>Cycling</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell>Activities</TableCell>
-                  <TableCell>Distance</TableCell>
-                  <TableCell>Activities</TableCell>
-                  <TableCell>Distance</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.years.map((year) => (
-                  <TableRow key={year.year}>
-                    <TableCell>{year.year}</TableCell>
-                    <TableCell>{year.statistics[0].n_activities}</TableCell>
-                    <TableCell>{formatDistance(year.statistics[0].total_distance)}</TableCell>
-                    <TableCell>{year.statistics[1].n_activities}</TableCell>
-                    <TableCell>{formatDistance(year.statistics[1].total_distance)}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
+    <Container maxWidth="lg">
+      <Box maxWidth="800px" margin="auto" marginTop="20px">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableBody>
+              <TableRow>
+                <TableCell>Total Activities</TableCell>
+                <TableCell>{data.n_activities}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Run Total Activities</TableCell>
+                <TableCell>{data.run_n_activities}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Run Total Distance</TableCell>
+                <TableCell>{formatDistance(data.run_total_distance)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Cycling Total Activities</TableCell>
+                <TableCell>{data.cycling_n_activities}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Cycling Total Distance</TableCell>
+                <TableCell>{formatDistance(data.cycling_total_distance)}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
-    </Flex>
+      <Box maxWidth="800px" margin="auto" marginTop="20px">
+        <Bar height={"100px"} options={weekOptions} data={weekData} />
+      </Box>
+      <Box maxWidth="400px" margin="auto" marginTop="20px">
+        <Performances performances={data.running_performances} />
+      </Box>
+      <Box maxWidth="800px" margin="auto" marginTop="20px">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Year</TableCell>
+                <TableCell colSpan={2}>Running</TableCell>
+                <TableCell colSpan={2}>Cycling</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell>Activities</TableCell>
+                <TableCell>Distance</TableCell>
+                <TableCell>Activities</TableCell>
+                <TableCell>Distance</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.years.map((year) => (
+                <TableRow key={year.year}>
+                  <TableCell>{year.year}</TableCell>
+                  <TableCell>{year.statistics[0].n_activities}</TableCell>
+                  <TableCell>{formatDistance(year.statistics[0].total_distance)}</TableCell>
+                  <TableCell>{year.statistics[1].n_activities}</TableCell>
+                  <TableCell>{formatDistance(year.statistics[1].total_distance)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Container>
   );
 };
 
