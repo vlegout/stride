d6222b2f176ed38f1c0cb2e1ce51d43667b63d8a
Vincent Legout <vincent@legout.info>
2025-06-06 20:37:14 +0200

    vite: remove unused publicDir config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react(), svgr()],
-    publicDir: mode === "development" ? "pub" : "public",
   };
 });
