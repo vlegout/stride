09e3fe217f491163376464b04aa72d01d78e791c
Vincent Legout <vincent@legout.info>
2025-06-27 13:18:14 +0200

    fix format

--- api/python/alembic/versions/f1d2a86022f0_remove_minutes_and_seconds_fields_from_.py
+++ api/python/alembic/versions/f1d2a86022f0_remove_minutes_and_seconds_fields_from_.py
@@ -5,6 +5,7 @@ Revises: c3bc7c6e9f0d
 Create Date: 2025-06-27 13:04:08.752082
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'f1d2a86022f0'
-down_revision: Union[str, Sequence[str], None] = 'c3bc7c6e9f0d'
+revision: str = "f1d2a86022f0"
+down_revision: Union[str, Sequence[str], None] = "c3bc7c6e9f0d"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -21,12 +22,12 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
     # Remove minutes and seconds fields from lap table
-    op.drop_column('lap', 'minutes')
-    op.drop_column('lap', 'seconds')
+    op.drop_column("lap", "minutes")
+    op.drop_column("lap", "seconds")
 
 
 def downgrade() -> None:
     """Downgrade schema."""
     # Add back minutes and seconds fields to lap table
-    op.add_column('lap', sa.Column('minutes', sa.Integer(), nullable=True))
-    op.add_column('lap', sa.Column('seconds', sa.Integer(), nullable=True))
+    op.add_column("lap", sa.Column("minutes", sa.Integer(), nullable=True))
+    op.add_column("lap", sa.Column("seconds", sa.Integer(), nullable=True))
